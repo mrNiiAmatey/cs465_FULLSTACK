fed15a8677d53d1726492a9741385dc524024fae
mrArmahtey <armahtey@gmail.com>
2025-04-11 23:48:56 -0500

    Module 5 completed Baseline

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var handlerbars = require('hbs');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -18,6 +19,9 @@ app.set('view engine', 'hbs');
 //register handlebars partials (https://www.npmjs.com/package/hbs)
 handlerbars.registerPartials(__dirname + '/app_server/views/partials');
 
+//Bring in the Database
+require('./app_api/models/db');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
